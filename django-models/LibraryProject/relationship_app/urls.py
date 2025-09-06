5758655ea5810ee89537a8a35844d66126060097
Martins Chukwudi <chukwudimartins39@gmail.com>
2025-09-06 12:06:20 +0100

    update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -24,3 +24,15 @@ urlpatterns = [
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="Library_detail"),
 
 ]
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("books/", views.list_books, name="list_books"),  # existing function-based view
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+]
