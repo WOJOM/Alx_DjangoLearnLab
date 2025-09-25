d812a2455f0b32dab297368604085d6e296afa6a
Martins Chukwudi <chukwudimartins39@gmail.com>
2025-09-25 22:51:58 +0100

    update

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
