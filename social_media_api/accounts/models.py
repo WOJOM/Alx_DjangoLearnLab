336e48a6bff0919e85226a135f884d2e544cab23
Martins Chukwudi <chukwudimartins39@gmail.com>
2025-10-08 03:06:27 +0100

    update

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,11 +1,16 @@
 # Create your models here.
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
-    profile_picture = models.URLField(blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
 
     def __str__(self):
         return self.username
