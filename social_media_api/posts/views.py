465375e0067e9db4bc147e9fb3380c42fd5c3f03
Martins Chukwudi <chukwudimartins39@gmail.com>
2025-10-09 21:17:45 +0100

    update

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -58,3 +58,26 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # ensure author is set to current user
         serializer.save(author=self.request.user)
+
+
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
